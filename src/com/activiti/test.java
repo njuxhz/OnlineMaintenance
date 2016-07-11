b77c58ac84a404698048ae60cd827f0a3841ffd1
njuxhz <qyeung@qq.com>
2016-07-12 03:05:15 +0800

    deploy test

--- src/com/activiti/test.java
+++ src/com/activiti/test.java
@@ -1,19 +1,39 @@
 package com.activiti;
 
-
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.ws.rs.core.Response;
 
+import org.apache.cxf.attachment.ContentDisposition;
 import org.apache.cxf.common.util.Base64Utility;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+import org.apache.cxf.jaxrs.ext.multipart.MultipartBody;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Client {
-	
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        WebClient client = createClient("repository/deployments");
+
+        InputStream resourceAsStream = test.class.getClassLoader().getResourceAsStream("diagrams/leave.bpmn");
+        client.type("multipart/form-data");
+        ContentDisposition cd = new ContentDisposition("form-data;name=bpmn;filename=leave.bpmn;");
+        Attachment att = new Attachment("leave.bpmn", resourceAsStream, cd);
+        MultipartBody body = new MultipartBody(att);
+        Response response = client.post(body);
+
+        // 转换并输出响应结果
+        printResult("部署流程", response);
+	}
+
 	public static final String BASE_REST_URL = "http://121.43.109.179/activiti-rest/service/";
 	private static ObjectMapper objectMapper = new ObjectMapper();
 	private static boolean formateOutputJson = true;
